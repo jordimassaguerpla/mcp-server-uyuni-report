3f73e45f55adf65a9165cbe45ac2e88c93edd05d
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2025-09-04 11:12:37 +0200

    load env when starting and inserting test data
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- insert_test_data.sh
+++ insert_test_data.sh
@@ -1,1 +1,2 @@
+. .env
 podman exec -ti reportdb psql reportdb -U $REPORT_DB_USER -h localhost -f /test/test_data.sql
--- start_uyuni_report_db.sh
+++ start_uyuni_report_db.sh
@@ -1,1 +1,2 @@
+. .env
 podman run --rm -ti -e POSTGRES_HOST_AUTH_METHOD=trust -e MANAGER_USER=not_needed -e MANAGER_PASS=not_needed -e MANAGER_DB_NAME=spacewalk  -e REPORT_DB_USER=$REPORT_DB_USER -e REPORT_DB_PASS=$REPORT_DB_PASS -e REPORT_DB_NAME=reportdb -p 5432:5432 --name=reportdb -v .:/test registry.opensuse.org/uyuni/server-postgresql:latest
